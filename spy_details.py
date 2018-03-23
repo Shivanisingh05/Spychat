127a998e92405858d3896c698bfa8067f4ecbb5f
Shivanisingh05 <shivanisingh8990@gmail.com>
2018-03-23 20:24:34 +0530

    update main and add spy file

---
+++ spy_details.py
@@ -0,0 +1,9 @@
+# default user
+
+spy_name= "James Bond"
+spy_salutation = "Mr."
+spy_rating = 4.5
+spy_age = 45
+
+print("Welcome " + spy_salutation + " " + spy_name + " your age is " + str(spy_age) + " and rating is " + str(
+    spy_rating) + "!")
